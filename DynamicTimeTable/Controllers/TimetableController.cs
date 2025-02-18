8eaab5fc40076effb64a160cd5373e6982a067a0
ratho <ratho@ARJUNSINH>
2025-02-18 22:39:37 +0530

    Add project files.

---
+++ DynamicTimeTable/Controllers/TimetableController.cs
@@ -0,0 +1,65 @@
+﻿using DynamicTimeTable.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicTimeTable.Controllers
+{
+    public class TimetableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new TimetableModel());
+        }
+        [HttpPost]
+        public IActionResult GenerateTimetable(TimetableModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            ViewBag.TotalHoursForWeek = model.TotalHoursForWeek;
+            model.SubjectHoursList = new List<SubjectHoursModel>(model.TotalSubjects);
+            return View("SubjectHours", model);
+        }
+        [HttpPost]
+        public IActionResult CreateTimetable(TimetableModel model)
+        {
+            int totalEnteredHours = model.SubjectHoursList.Sum(s => s.Hours);
+
+            if (totalEnteredHours != model.TotalHoursForWeek)
+            {
+                ModelState.AddModelError("", "Total subject hours must match the total hours for the week.");
+                return View("SubjectHours", model);
+            }
+
+            model.GeneratedTimetable = GenerateSchedule(model);
+            return View("Timetable", model);
+        }
+        private List<List<string>> GenerateSchedule(TimetableModel model)
+        {
+            var timetable = new List<List<string>>();
+            var subjects = new List<string>();
+
+            foreach (var subject in model.SubjectHoursList)
+            {
+                subjects.AddRange(Enumerable.Repeat(subject.SubjectName, subject.Hours));
+            }
+
+            var random = new Random();
+            subjects = subjects.OrderBy(x => random.Next()).ToList();
+
+            for (int i = 0; i < model.SubjectsPerDay; i++)
+            {
+                var row = new List<string>();
+                for (int j = 0; j < model.WorkingDays; j++)
+                {
+                    row.Add(subjects.FirstOrDefault() ?? "N/A");
+                    subjects.RemoveAt(0);
+                }
+                timetable.Add(row);
+            }
+
+            return timetable;
+        }
+    }
+}
---
+++ DynamicTimeTable/Models/TimetableModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DynamicTimeTable.Models
+{
+    public class TimetableModel
+    {
+        [Required]
+        [Range(1, 7, ErrorMessage = "Enter a value between 1 and 7")]
+        public int WorkingDays { get; set; }
+
+        [Required]
+        [Range(1, 8, ErrorMessage = "Enter a value less than 9")]
+        public int SubjectsPerDay { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Total subjects must be positive")]
+        public int TotalSubjects { get; set; }
+
+        public int TotalHoursForWeek => WorkingDays * SubjectsPerDay;
+
+        public List<SubjectHoursModel> SubjectHoursList { get; set; } = new List<SubjectHoursModel>();
+        public List<List<string>>? GeneratedTimetable { get; set; }
+    }
+    public class SubjectHoursModel
+    {
+        public string SubjectName { get; set; }
+        public int Hours { get; set; }
+    }
+}
